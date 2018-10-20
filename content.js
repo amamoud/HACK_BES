baa7673cb84826dc15e8a4e8389e323a7a0f132b
mngarcia <43502766+mngarcia@users.noreply.github.com>
2018-10-20 15:04:13 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
 		var img =
 			 	document.createElement("img");
  
-				img.src = "memes/midlife_crisis1.jpg";
+				img.src = "midlife_crisis1.jpg";
 				var src = document.getElementById("ig1");
 			// , 
 			// {
@@ -17,102 +17,41 @@
 			
 		var quoteSource=[
 		{
-			quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
-			name:"Francis of Assisi"
+			quote: "Don’t stress. Do your best. Forget the rest.",
+			name:"Anonymous"
 	    },
 	    {
-	    	quote:"Believe you can and you're halfway there.",
-	    	name:"Theodore Roosevelt"
+	    	quote:"I’m not telling you it’s going to be easy- I'm telling you it’s going to be worth it.",
+	    	name:"Art Williams"
 	    },
 	    {
-	    	quote:"It does not matter how slowly you go as long as you do not stop.",
-	    	name:"Confucius"
+	    	quote:"Foster collaboration, not competition.",
+	    	name:"Anonymous"
 	    },
 	    {
-	    	quote:"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
-	    	name:"Thomas A. Edison"
+	    	quote:"Inclusion is not a matter of political correctness. It is the key to growth.",
+	    	name:"Jesse Jackson, Politicial and Civil Rights Activist"
 	    },
 	    {
-	    	quote:"The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence.",
-	    	name:"Confucius"
+	    	quote:"When we listen and celebrate what is both common and different, we become a wiser, more inclusive, and better organization.",
+	    	name:"Pat Wadors, Head of HR at LinkedIn"
 	    },
 	    {
-	    	quote:"Don't watch the clock; do what it does. Keep going.",
-	    	name:"Sam Levenson"
+	    	quote:"The point isn’t to get people to accept that they have biases, but to get them to see [for themselves] that those biases have negative consequences for others.",
+	    	name:"Theresa McHenry, HR Director at Microsoft UK"
 	    },
 	    {
-	    	quote:"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
-	    	name:"Ayn Rand"
+	    	quote:"You are trapped by nothing more than a poor attitude",
+	    	name:"Sid Mittra, To Bee or Not to Bee: Winning Against All Odds"
 	    },
 	    {
-	    	quote:"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
-	    	name:"Ayn Rand"
+	    	quote:"Imagine what our story would look like if, rather than succumbing to the insistent voices of family or culture, we determined that our vocation was to be a better human.",
+	    	name:"James Hollis, Ph.D."
 	    },
 	    {
-	    	quote:"Expect problems and eat them for breakfast.",
-	    	name:"Alfred A. Montapert"
-	    },
-	    {
-	    	quote:"Start where you are. Use what you have. Do what you can.",
-	    	name:"Arthur Ashe"
-	    },
-	    {
-	    	quote:"Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
-	    	name:"Samuel Beckett"
-	    },
-	    {
-	    	quote:"Be yourself; everyone else is already taken.",
-	    	name:"Oscar Wilde"
-	    },
-	    {
-	    	quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-	    	name:"Albert Einstein"
-	    },
-	    {
-	    	quote:"Always remember that you are absolutely unique. Just like everyone else.",
-	    	name:"Margaret Mead"
-	    },
-	    {
-	    	quote:"Do not take life too seriously. You will never get out of it alive.",
-	    	name:"Elbert Hubbard"
-	    },
-	    {
-	    	quote:"People who think they know everything are a great annoyance to those of us who do.",
-	    	name:"Isaac Asimov"
-	    },
-	    {
-	    	quote:"Procrastination is the art of keeping up with yesterday.",
-	    	name:"Don Marquis"
-	    },
-	    {
-	    	quote:"Get your facts first, then you can distort them as you please.",
-	    	name:"Mark Twain"
-	    },
-	    {
-	    	quote:"A day without sunshine is like, you know, night.",
-	    	name:"Steve Martin"
-	    },
-	    {
-	    	quote:"My grandmother started walking five miles a day when she was sixty. She's ninety-seven now, and we don't know where the hell she is.",
-	    	name:"Ellen DeGeneres"
-	    },
-	    {
-	    	quote:"Don't sweat the petty things and don't pet the sweaty things.",
-	    	name:"George Carlin"
-	    },
-	    {
-	    	quote:"Always do whatever's next.",
-	    	name:"George Carlin"
-	    },
-	    {
-	    	quote:"Atheism is a non-prophet organization.",
-	    	name:"George Carlin"
-	    },
-	    {
-	    	quote:"Hapiness is not something ready made. It comes from your own actions.",
-	    	name:"Dalai Lama"
-	    }
-
+	    	quote:"You don’t have to live your life the way other people expect you to.",
+	    	name:"Chris Guillebeau"
+	    }	    
 	];
 		
 
